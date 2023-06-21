93c3af86bbb7b48c02f029e35aa0494d087fd08e
mooky <mooky1007@naver.com>
2023-06-21 20:46:46 +0900

    share

--- src/page/TestPage.mjs
+++ src/page/TestPage.mjs
@@ -11,13 +11,15 @@ class TestPage {
         this.question = this.data.data;
         this.result = this.data.result;
 
-        if(this.data.type === 'point'){
+        if (this.data.type === 'point') {
             this.point = 0;
             this.type = data.type;
         }
-        if(this.data.type === 'indicator'){
+        if (this.data.type === 'indicator') {
             this.indicator = {};
-            this.indicatorType = Object.values(this.question).map(({type}) => type).filter((el, idx, arr) => arr.indexOf(el) === idx);
+            this.indicatorType = Object.values(this.question)
+                .map(({ type }) => type)
+                .filter((el, idx, arr) => arr.indexOf(el) === idx);
             this.type = data.type;
         }
     }
@@ -33,22 +35,34 @@ class TestPage {
         this.question = this.data.data;
         this.result = this.data.result;
 
-        if(this.data.type === 'point'){
+        if (this.data.type === 'point') {
             this.point = 0;
         }
-        if(this.data.type === 'indicator'){
+        if (this.data.type === 'indicator') {
             this.indicator = {};
-            this.indicatorType = Object.values(this.question).map(({type}) => type).filter((el, idx, arr) => arr.indexOf(el) === idx);
+            this.indicatorType = Object.values(this.question)
+                .map(({ type }) => type)
+                .filter((el, idx, arr) => arr.indexOf(el) === idx);
         }
 
-        const viewContainer = createElement('div', {attribute: {class: 'view_container'}});
-        const contentWrap = createElement('div', {attribute: {class: 'content_wrap'}});
-        const title = createElement('div', {content: this.title, attribute: {class: 'title'}});
-        const description = createElement('div', {content: this.desc, attribute: {class: 'description'}});
-        const information = createElement('ul', {attribute: {class: 'information'}});
-        const questionLength = createElement('li', {content: `<span>총 문항 :</span><span id="question_length">${this.testLength}문항</span>`, attribute: {class: 'question_length'}});
-        const time = createElement('li', {content: `<span>소요시간 :</span><span id="time">${Math.trunc(this.testLength * 10 / 60) > 0 ? Math.trunc(this.testLength * 10 / 60) + ' ~ ' + Math.trunc(this.testLength * 20 / 60) + '분' : '1분 이내'}</span>`, attribute: {class: 'time'}});
-        const buttonWrap = createElement('div', {attribute: {class: 'button_wrap'}});
+        const viewContainer = createElement('div', { attribute: { class: 'view_container' } });
+        const contentWrap = createElement('div', { attribute: { class: 'content_wrap' } });
+        const title = createElement('div', { content: this.title, attribute: { class: 'title' } });
+        const description = createElement('div', { content: this.desc, attribute: { class: 'description' } });
+        const information = createElement('ul', { attribute: { class: 'information' } });
+        const questionLength = createElement('li', {
+            content: `<span>총 문항 :</span><span id="question_length">${this.testLength}문항</span>`,
+            attribute: { class: 'question_length' },
+        });
+        const time = createElement('li', {
+            content: `<span>소요시간 :</span><span id="time">${
+                Math.trunc((this.testLength * 10) / 60) > 0
+                    ? Math.trunc((this.testLength * 10) / 60) + ' ~ ' + Math.trunc((this.testLength * 20) / 60) + '분'
+                    : '1분 이내'
+            }</span>`,
+            attribute: { class: 'time' },
+        });
+        const buttonWrap = createElement('div', { attribute: { class: 'button_wrap' } });
 
         information.append(questionLength, time);
         contentWrap.append(title, description, information);
@@ -62,11 +76,11 @@ class TestPage {
         this.current = 0;
         this.point = 0;
         this.indicator = {};
-        
+
         this.DOM.querySelector('.information').style.display = 'block';
 
         this.DOM.querySelector('.button_wrap').innerHTML = '';
-        const startBtn = createElement('button', {content: this.startButton || "시작하기", attribute: {class: 'start_btn'}});
+        const startBtn = createElement('button', { content: this.startButton || '시작하기', attribute: { class: 'start_btn' } });
         startBtn.addEventListener('click', () => this.start());
         this.DOM.querySelector('.button_wrap').appendChild(startBtn);
     }
@@ -76,9 +90,10 @@ class TestPage {
     }
 
     next() {
-        if(this.current === this.testLength - 1){
-            this.getResult(); return
-        };
+        if (this.current === this.testLength - 1) {
+            this.getResult();
+            return;
+        }
         this.current++;
         this.renderQuestion();
     }
@@ -89,72 +104,95 @@ class TestPage {
         this.DOM.querySelector('.description').innerHTML = this.question[this.current].question;
         this.DOM.querySelector('.button_wrap').innerHTML = '';
 
-        for(let i = 0; i < this.question[this.current].answerList.length; i++){
+        for (let i = 0; i < this.question[this.current].answerList.length; i++) {
             const button = document.createElement('button');
             button.innerHTML = this.question[this.current].answerList[i].answer;
             button.addEventListener('click', () => {
-                if(this.type === "point") this.point += this.question[this.current].answerList[i].point;
-                if(this.type === "indicator"){
+                if (this.type === 'point') this.point += this.question[this.current].answerList[i].point;
+                if (this.type === 'indicator') {
                     this.indicator[this.question[this.current].answerList[i].indicator]
-                    ? this.indicator[this.question[this.current].answerList[i].indicator] += 1
-                    : this.indicator[this.question[this.current].answerList[i].indicator] = 1;
+                        ? (this.indicator[this.question[this.current].answerList[i].indicator] += 1)
+                        : (this.indicator[this.question[this.current].answerList[i].indicator] = 1);
                 }
-                this.next()
+                this.next();
             });
             this.DOM.querySelector('.button_wrap').appendChild(button);
         }
     }
 
     getResult() {
-        if(this.type === "point"){
-            this.userResult = "";
-            this.userResult = this.result.filter(({point}) => {
-                if(point.split("-").length === 1){
+        if (this.type === 'point') {
+            this.userResult = '';
+            this.userResult = this.result.filter(({ point }) => {
+                if (point.split('-').length === 1) {
                     return this.point === +point ? true : false;
                 }
-                const [min, max] = point.split("-");
+                const [min, max] = point.split('-');
                 return this.point >= +min && this.point <= +max ? true : false;
             })[0];
-
         }
 
-        if(this.type === "indicator" && this.current === this.testLength - 1){
-            this.userResult = "";
-            this.indicatorType.forEach(type => {
-                const [type1, type2] = type.split("");
+        if (this.type === 'indicator' && this.current === this.testLength - 1) {
+            this.userResult = '';
+            this.indicatorType.forEach((type) => {
+                const [type1, type2] = type.split('');
 
-                this.indicator[type1] === undefined ? this.indicator[type1] = 0 : this.indicator[type1];
-                this.indicator[type2] === undefined ? this.indicator[type2] = 0 : this.indicator[type2];
+                this.indicator[type1] === undefined ? (this.indicator[type1] = 0) : this.indicator[type1];
+                this.indicator[type2] === undefined ? (this.indicator[type2] = 0) : this.indicator[type2];
 
-                if(this.indicator[type1] >= this.indicator[type2]){
+                if (this.indicator[type1] >= this.indicator[type2]) {
                     this.userResult += type1;
-                }else if(this.indicator[type1] < this.indicator[type2]){
+                } else if (this.indicator[type1] < this.indicator[type2]) {
                     this.userResult += type2;
                 }
             });
 
-            this.userResult = this.result.filter(({indicator}) => {
+            this.userResult = this.result.filter(({ indicator }) => {
                 return indicator === this.userResult;
             })[0];
         }
 
-
         this.DOM.querySelector('.title').innerHTML = this.userResult.title;
         this.DOM.querySelector('.description').innerHTML = this.userResult.description;
-        
+
         this.DOM.querySelector('.button_wrap').innerHTML = '';
         const replayBtn = document.createElement('button');
         replayBtn.innerHTML = '다시하기';
         replayBtn.addEventListener('click', () => this.restart());
 
-        const listBtn = createElement('button', {content: "목록으로"})
+        const listBtn = createElement('button', { content: '목록으로' });
 
         listBtn.addEventListener('click', () => {
             const event = new CustomEvent('goToHome');
-            this.DOM.dispatchEvent(event)
-        })
+            this.DOM.dispatchEvent(event);
+        });
 
-        this.DOM.querySelector('.button_wrap').append(replayBtn, listBtn);
+        const shareBtn = createElement('button', { content: '공유하기', attribute: { id: 'kakaotalk-sharing-btn' } });
+
+        this.DOM.querySelector('.button_wrap').append(replayBtn, shareBtn, listBtn);
+
+        Kakao.Share.createDefaultButton({
+            container: '#kakaotalk-sharing-btn',
+            objectType: 'feed',
+            content: {
+                title: this.userResult.title,
+                description: this.userResult.description.slice(0, 50).replace(/<br \/>/g, ' ') + '...',
+                imageUrl: 'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+                link: {
+                    mobileWebUrl: 'https://developers.kakao.com',
+                    webUrl: 'https://developers.kakao.com',
+                },
+            },
+            buttons: [
+                {
+                    title: '테스트 하러가기',
+                    link: {
+                        mobileWebUrl: 'https://mooky1007.github.io/try-test/',
+                        webUrl: 'https://mooky1007.github.io/try-test/',
+                    },
+                }
+            ],
+        });
         return;
     }
 
